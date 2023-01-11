0dffbc2cbfb36efeb923f54deb99f9247b95619f
ivan <d1zzzy.project@gmail.com>
2023-01-11 21:06:07 +0300

    add controle

--- user.py
+++ user.py
@@ -5,25 +5,26 @@ class User(pygame.sprite.Sprite):
     def __init__(self, image, xCord, yCord, *group):
         super().__init__(*group)
         self.image = image
-        self.xCord = xCord
-        self.yCord = yCord
         self.rect = self.image.get_rect()
+        self.rect.x = xCord
+        self.rect.y = yCord
         self.speed = 3
         self.jumpCount = 10  # высота прыжка
         self.isJump = False
         self.hp = 100
 
     def controle(self, event):
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-            self.xCord -= self.speed
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
-            self.xCord += self.speed
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_d]:
+            self.rect.x += self.speed
         if not(self.isJump):
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            if keys[pygame.K_SPACE]:
                 self.isJump = True
         else:
             if self.jumpCount >= -10:
-                self.yCord -= (self.jumpCount * abs(self.jumpCount)) * 0.5
+                self.rect.y -= (self.jumpCount * abs(self.jumpCount)) * 0.5
                 self.jumpCount -= 1
             else:
                 self.jumpCount = 10
