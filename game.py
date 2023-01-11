6c53f106e9dc808c62f288973ad9733fa5615846
ivan <d1zzzy.project@gmail.com>
2023-01-11 21:44:07 +0300

    last update

--- game.py
+++ game.py
@@ -2,31 +2,35 @@ import sys
 import os
 import pygame
 from menu import Menu
-from user import User
+from player import Player
+
+
+def load_image(self, name, colorkey=None):
+    fullname = os.path.join('date', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
 
 class Game:
-
     def __init__(self, size_screen, caption):
         pygame.display.set_caption(caption)
         self.screen = pygame.display.set_mode(size_screen)
         self.size_screen = size_screen
         self.open_menu('main')
 
-    def load_image(self, name, colorkey=None):
-        fullname = os.path.join('date', name)
-        # если файл не существует, то выходим
-        if not os.path.isfile(fullname):
-            print(f"Файл с изображением '{fullname}' не найден")
-            sys.exit()
-        image = pygame.image.load(fullname)
-        if colorkey is not None:
-            image = image.convert()
-            if colorkey == -1:
-                colorkey = image.get_at((0, 0))
-            image.set_colorkey(colorkey)
-        else:
-            image = image.convert_alpha()
-        return image
+    def create_charackter(self):
+        pass
 
     def start_game(self):
             FPS = 60
@@ -34,24 +38,31 @@ class Game:
             clock = pygame.time.Clock()
             all_sprites = pygame.sprite.Group()
             player_group = pygame.sprite.Group()
-            image = self.load_image("1.png", -1)
-            User(image, 500, 500, player_group, all_sprites)
+            image = load_image("1.png", -1)
+            player_1_controle = {
+                'left': pygame.K_a,
+                'right': pygame.K_d,
+                'jump': pygame.K_SPACE,
+                'attack': None
+
+            }
+            Player(player_1_controle, 500, 500, self.player_group, self.all_sprites)
             run = True
             while run:
                 for event in pygame.event.get():  # error is here
                     if event.type == pygame.QUIT:
                         self.exit_game(event)
                         run = False
-                    all_sprites.update(event)
+                self.all_sprites.update()
                 self.screen.fill((255, 255, 255))
-                all_sprites.draw(self.screen)
+                self.all_sprites.draw(self.screen)
                 tick += 1
                 clock.tick(FPS)
                 pygame.display.flip()
             pygame.quit()
 
-    def open_menu(self, type):
-        Menu(type).start_menu(self.size_screen, self.screen, self.start_game)
+    def open_menu(self, type_menu):
+        Menu(type_menu).start_menu(self.size_screen, self.screen, self.start_game)
 
     def exit_game(self, event):
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
